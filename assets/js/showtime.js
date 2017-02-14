8c6357fd94f2bb00c511bb16c18d4bfd26bdfd4d
Sun Qingyao <sunqingyao19970825@gmail.com>
2017-02-14 13:51:48 +0800

    stop using jekyll

--- assets/js/showtime.js
+++ assets/js/showtime.js
@@ -1,19 +1,27 @@
 var update;
 
 (update = function() {
+    var now = moment().format('X');
     document.getElementById("sv")
-        .innerHTML = ((moment("20170628 10:00", "YYYYMMDD hh:mm").format('x')
-                      - moment().format('x')) / 1000).toFixed(3);
+        .innerHTML = moment("20170628 10:00", "YYYYMMDD hh:mm")
+                     .format('X') - now;
+    /*   moment("20170628 08:00", "YYYYMMDD hh:mm").format('X')
+     * - moment("20170214 08:00", "YYYYMMDD hh:mm").format('X')
+     * === 115776
+     * */
+    document.getElementById("bar")
+        .style.width = (100 - (moment("20170628 10:00", "YYYYMMDD hh:mm")
+        .format('X') - now) / 115776).toString() + "%";
     document.getElementById("ma")
-        .innerHTML = ((moment("20170619 08:00", "YYYYMMDD hh:mm").format('x')
-                      - moment().format('x')) / 1000).toFixed(3);
+        .innerHTML = moment("20170619 08:00", "YYYYMMDD hh:mm")
+                     .format('X') - now;
     document.getElementById("aa")
-        .innerHTML = ((moment("20170628 08:00", "YYYYMMDD hh:mm").format('x')
-                      - moment().format('x')) / 1000).toFixed(3);
+        .innerHTML = moment("20170628 08:00", "YYYYMMDD hh:mm")
+                     .format('X') - now;
     document.getElementById("me")
-        .innerHTML = ((moment("20170627 08:00", "YYYYMMDD hh:mm").format('x')
-                      - moment().format('x')) / 1000).toFixed(3);
+        .innerHTML = moment("20170627 08:00", "YYYYMMDD hh:mm")
+                     .format('X') - now;
 })();
 
-setInterval(update, 1);
+setInterval(update, 1000);
 
